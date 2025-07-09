50faf83f5a630aeb4679e438d264a3c785e0e208
mhmendez <martinhms2015>
2025-07-09 18:32:28 -0300

    Initial Commit

---
+++ IosApp/IosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosAppApp.swift
+//  IosApp
+//
+//  Created by Martin Horacio Mendez on 09/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct IosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
